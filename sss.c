5cb54d3ed80cbbde07bd962f96f5de704e27b250
cmlesquivel <anayacamilo@yahoo.es>
2019-11-18 18:06:57 +0000

    malloc fixed

--- sss.c
+++ sss.c
@@ -9,6 +9,7 @@ int main (void)
     size_t numberbytes;
     char *string;
     const char s[2] = " \n";
+    char **argv;
     char *token;
     int tahv;
 
@@ -18,21 +19,22 @@ int main (void)
     printf("$ ");
 
     readed_bytes = getline(&string, &numberbytes, stdin);
-    char *argv[4];
+
+    argv = malloc(readed_bytes * sizeof *argv);
     if (readed_bytes == -1)
     {
         printf("Error");
     }
     else
     {
-		tahv = 0;
+	tahv = 0;
         token = strtok(string, s);
         while(token)
         {
             argv[tahv++] = token;
             token = strtok(NULL, s);
 		}
-		argv[3] = 0;
+		argv[tahv] = 0;
 		if (execve(argv[0], argv, NULL) == -1)
 		{
 			perror("Error: \n");
